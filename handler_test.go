ca40385d3324d09f251dd89236182c349f93231b
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-10-11 18:27:47 +0900

    fixed handler test

--- handler_test.go
+++ handler_test.go
@@ -1,31 +1,63 @@
-package router
+package router_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/Code-Hex/go-router-simple"
 )
 
 func TestHandler(t *testing.T) {
-	router := NewRouter()
-	router.HandleFunc("GET",
-		"/blog/:year/:month",
-		func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
-			year := ParamFromContext(ctx, "year")
-			month := ParamFromContext(ctx, "month")
-			t.Log(year)
-			t.Log(month)
-			w.Write([]byte("OK"))
-		},
-	)
+	r := router.New()
 
-	srv := httptest.NewServer(router)
+	r.GET("/", http.HandlerFunc(Index))
+	r.GET("/hi/:name", http.HandlerFunc(Hi))
+	r.GET("/hi/{name}", http.HandlerFunc(Hi))
+	r.GET("/download/*.*", http.HandlerFunc(GetFilename))
+	r.GET(`/blog/{year:\d{4}}/{month:(?:\d{2})}`, Blog())
 
-	resp, err := http.Get(srv.URL + "/blog/2020/10")
-	if err != nil {
-		t.Fatal(err)
+	srv := httptest.NewServer(r)
+
+	for _, path := range []string{
+		"/",
+		"/hi/codehex",
+		"/hi/taro",
+		"/download/file.xml",
+		"/download/filename.json",
+		"/blog/2020/10",
+	} {
+		resp, err := http.Get(srv.URL + path)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp.Body.Close()
+		if resp.StatusCode != 200 {
+			t.Fatalf("path: %q, code: %d", path, resp.StatusCode)
+		}
 	}
-	defer resp.Body.Close()
-	t.Log(resp.Status)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Index!\n")
+}
+
+func Hi(w http.ResponseWriter, r *http.Request) {
+	name := router.ParamFromContext(r.Context(), "name")
+	fmt.Fprintf(w, "Welcome! %q\n", name)
+}
+
+func GetFilename(w http.ResponseWriter, r *http.Request) {
+	wildcards := router.WildcardsFromContext(r.Context())
+	fmt.Fprintf(w, "File: %q\n", strings.Join(wildcards, "."))
+}
+
+func Blog() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		year := router.ParamFromContext(r.Context(), "year")
+		month := router.ParamFromContext(r.Context(), "month")
+		fmt.Fprintf(w, "Render: %q\n", year+"/"+month)
+	})
 }
