12e70d02dbee414eb3300dc7d2081643add2db04
Kei Kamikawa <code-hex@users.noreply.github.com>
2020-10-11 12:58:23 +0900

    added some handlers for shorthand

--- handler.go
+++ handler.go
@@ -21,10 +21,27 @@ func NewRouter() *Router {
 	}
 }
 
+func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := req.URL.Path
+	rcs := r.methodMap[req.Method]
+	for _, rc := range rcs {
+		params, err := rc.MatchPath(path)
+		if err != nil {
+			if err != Errdidnotmatch {
+				log.Printf("error: %q", err)
+			}
+			continue
+		}
+		ctx := contextWithParams(req.Context(), params)
+		rc.handler.ServeHTTP(w, req.WithContext(ctx))
+		return
+	}
+}
+
 // Handle handles handler
 func (r *Router) Handle(method, path string, handler http.Handler) error {
 	regex, captures := createPathMatcher(path)
-	rg, err := regexp.Compile(regex)
+	rg, err := regexp.MustCompile(regex)
 	if err != nil {
 		return err
 	}
@@ -41,21 +58,49 @@ func (r *Router) HandleFunc(method, path string, handler func(http.ResponseWrite
 	return r.Handle(method, path, http.HandlerFunc(handler))
 }
 
-func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	path := req.URL.Path
-	rcs := r.methodMap[req.Method]
-	for _, rc := range rcs {
-		params, err := rc.MatchPath(path)
-		if err != nil {
-			if err != Errdidnotmatch {
-				log.Printf("error: %q", err)
-			}
-			continue
-		}
-		ctx := contextWithParams(req.Context(), params)
-		rc.handler.ServeHTTP(w, req.WithContext(ctx))
-		return
-	}
+// GET is a shorthand for router.Handle(http.MethodGet, path, handle)
+func (r *Router) GET(path string, handler http.Handler) error {
+	return r.Handle(http.MethodGet, path, handler)
+}
+
+// HEAD is a shorthand for router.Handle(http.MethodHead, path, handle)
+func (r *Router) HEAD(path string, handler http.Handler) error {
+	return r.Handle(http.MethodHead, path, handler)
+}
+
+// POST is a shorthand for router.Handle(http.MethodPost, path, handle)
+func (r *Router) POST(path string, handler http.Handler) error {
+	return r.Handle(http.MethodPost, path, handler)
+}
+
+// PUT is a shorthand for router.Handle(http.MethodPut, path, handle)
+func (r *Router) PUT(path string, handler http.Handler) error {
+	return r.Handle(http.MethodPut, path, handler)
+}
+
+// PATCH is a shorthand for router.Handle(http.MethodPatch, path, handle)
+func (r *Router) PATCH(path string, handler http.Handler) error {
+	return r.Handle(http.MethodPatch, path, handler)
+}
+
+// DELETE is a shorthand for router.Handle(http.MethodDelete, path, handle)
+func (r *Router) DELETE(path string, handler http.Handler) error {
+	return r.Handle(http.MethodDelete, path, handler)
+}
+
+// CONNECT is a shorthand for router.Handle(http.MethodConnect, path, handle)
+func (r *Router) CONNECT(path string, handler http.Handler) error {
+	return r.Handle(http.MethodConnect, path, handler)
+}
+
+// OPTIONS is a shorthand for router.Handle(http.MethodOptions, path, handle)
+func (r *Router) OPTIONS(path string, handler http.Handler) error {
+	return r.Handle(http.MethodOptions, path, handler)
+}
+
+// TRACE is a shorthand for router.Handle(http.MethodTrace, path, handle)
+func (r *Router) TRACE(path string, handler http.Handler) error {
+	return r.Handle(http.MethodTrace, path, handler)
 }
 
 type paramsKey struct{}
